fbc4685ab668f4f8146690c896c0c92075d2aca7
Manuel Fernando <manuelfernandocr4@gmail.com>
2026-09-05 15:33:59 -0300

    feat: centraliza estado dos produtos

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import Produtos from "./pages/Produtos/Produtos.jsx";
 import Estoque from "./pages/Estoque/Estoque.jsx";
 import Relatorios from "./pages/Relatorios/Relatorios.jsx";
 import Movimentacoes from "./pages/Movimentacoes/Movimentacoes.jsx";
+import produtosIniciais from "./data/produtos.js";
+import { useState } from "react";
 
 export default function App() {
+  const [produtos, setProduto] = useState(produtosIniciais);
+  console.log(produtos);
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/produtos" element={<Produtos />} />
-      <Route path="/estoque" element={<Estoque />} />
+      <Route path="/produtos" element={<Produtos produtos={produtos} />} />
+      <Route path="/estoque" element={<Estoque produtos={produtos} />} />
       <Route path="/relatorios" element={<Relatorios />} />
       <Route path="/movimentacoes" element={<Movimentacoes />} />
     </Routes>
